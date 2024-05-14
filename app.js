e04387b1710f2d753103da979cf676a013f8904f
hariniraj2005 <165803958+hariniraj2005@users.noreply.github.com>
2024-05-14 12:47:18 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,21 +1,35 @@
 const express=require('express');
-
-const bodyParser=require('body-parser');
-
+const fs=require('fs');
 const app=express();
-
-const adminRoutes=require('./router/login');
-
-const shopRoutes=require('./router/text');
-
-app.use(bodyParser.urlencoded({extended:false}));
-
-app.use('/admin',adminRoutes);
-
-app.use('/shop',shopRoutes);
-
-app.use((req,res,next)=>{
-  res.status(404).send('<h1>Page not Found</h1>');
+const bodyParser=require('body-parser');
+app.use(bodyParser.urlencoded({extended : false}));
+app.get('/',(req,res)=>{
+  fs.readFile("message.txt",(err,data)=>{
+      if(err){
+        console.log(err);
+        data="no chat exists"
+      }
+      res.send(`${data}<form action="/" onSubmit="document.getElementById('username').value=localStorage.getItem('username')" method="POST">
+          <label for="message"></label>
+          <input type="text" id="message" name="message" placeHolder="message">
+          <input type="hidden" id="username" name="username"<br>
+          <button type="submit">Send</button> 
+           </form>`);
 })
+  })
+  
+app.post('/', (req, res) => {
+  console.log(req.body.username);
+  console.log(req.body.message);
+ fs.writeFile("message.txt",`${req.body.username}:${req.body.message}`,{flag:'a'},(err)=>err?console.log(err) : res.redirect('/'));
+ });
+ 
+ app.get('/login', (req, res) => {
+  res.send(`<form onSubmit="localStorage.setItem('username', document.getElementById('username').value)" action="/login" method="POST"><input id="username" type="text" name="username"><br><button type="submit">add</button></form>`);
+});
 
+app.post('/login', (req, res) => {
+  res.redirect('/');
+});
+  
 app.listen(3000);
